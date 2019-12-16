266f5744eca63f0d533876ff7fae9138ff40f073
Wolfgang Becker <wolfgang@cobalt.io>
2019-12-16 23:58:27 +0100

    Refactor groups logic

--- src/lambda/http/getGroups.ts
+++ src/lambda/http/getGroups.ts
@@ -1,19 +1,12 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
-
-const groupsTable = process.env.GROUPS_TABLE
+import { getAllGroups } from '../../businessLogic/groups';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
 
-  const result = await docClient.scan({
-    TableName: groupsTable
-  }).promise()
-
-  const items = result.Items
+  const items = await getAllGroups();
 
   return {
     statusCode: 200,
